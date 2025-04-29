ff14b75b93dee7826fc6c8fa3c0fcd0b7e35c9b9
3bdop <abdbinm55@gmail.com>
2025-04-29 12:31:05 +0300

    remove db connection to db_services

--- src/main.py
+++ src/main.py
@@ -5,13 +5,10 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
 from src.api.endpoints import router
-from src.config import MONGODB_DB_NAME, MONGODB_URI
-from src.utils.db_services import connect_to_mongo
 
 app = FastAPI(title="Ebla ChatBot Assistant")
 app.include_router(router)
 
-db = connect_to_mongo(MONGODB_URI, MONGODB_DB_NAME)
 BASE_DIR = Path(__file__).parent
 
 # Mount static files (CSS, images)
