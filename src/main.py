051fd52fe4fc5122baffee000c114eedbaad31c6
3bdop <abdbinm55@gmail.com>
2025-04-29 09:54:08 +0300

    remove unused coed

--- src/main.py
+++ src/main.py
@@ -13,13 +13,9 @@ app.include_router(router)
 
 db = connect_to_mongo(MONGODB_URI, MONGODB_DB_NAME)
 BASE_DIR = Path(__file__).parent
-# STATIC_DIR = BASE_DIR / "static"
-# TEMPLATES_DIR = BASE_DIR / "templates"
 
 # Mount static files (CSS, images)
 app.mount("/static", StaticFiles(directory=BASE_DIR / "static"), name="static")
-print(BASE_DIR)
-# Setup templates
 
 # Allow CORS for frontend applications
 app.add_middleware(
