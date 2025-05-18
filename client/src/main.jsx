3c44817c37aa624e644f1888ba27edf09970709f
3bdop <abdbinm55@gmail.com>
2025-05-18 10:16:59 +0300

    refactor App.jsx to use UI, Experience and Avatar components

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './App.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ChatProvider } from "./hooks/useChat";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ChatProvider>
+      <App />
+    </ChatProvider>
+  </React.StrictMode>
+);
